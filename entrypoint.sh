c62555ab98b43c85fe709c623491c088d7694201
Iván Todorovich <ivan.todorovich@gmail.com>
2024-08-29 17:17:51 -0300

    ℹ️ fix and improve the welcome message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ run-parts --exit-on-error --report $RESOURCES/entrypoint.d
 
 echo "Running command: $@"
 case "$1" in
+    # Simulate a login shell
+    bash)
+        exec bash -l -c "$@"
+        ;;
+    # Launch Odoo passing through the provided arguments
     --)
         shift
         exec odoo "$@"
@@ -13,6 +18,7 @@ case "$1" in
     -*)
         exec odoo "$@"
         ;;
+    # Run the provided command
     *)
         exec "$@"
 esac
