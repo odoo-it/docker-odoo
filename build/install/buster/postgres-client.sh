39faee22a6c785fcf5affd454f143c22b54d7275
Iván Todorovich <ivan.todorovich@gmail.com>
2024-10-14 17:01:09 -0300

    🐛 use postgres apt archive for buster

--- build/install/buster/postgres-client.sh
+++ build/install/buster/postgres-client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIBUTION-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
+echo "deb https://apt-archive.postgresql.org/pub/repos/apt $DISTRIBUTION-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
 curl -SL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get install -yqq --no-install-recommends postgresql-client
