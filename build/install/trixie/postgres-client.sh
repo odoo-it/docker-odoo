65fbfd67cf7cde279e78901e20e6bc0545863a00
Iván Todorovich <ivan.todorovich@gmail.com>
2025-09-19 13:56:45 -0300

    fix trixie psql

--- build/install/trixie/postgres-client.sh
+++ build/install/trixie/postgres-client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIBUTION-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
-curl -SL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb  [arch=${TARGETPLATFORM/linux\//} signed-by=/etc/apt/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt/ ${DISTRIBUTION}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc -o /etc/apt/keyrings/postgresql.asc
 apt-get update
 apt-get install -yqq --no-install-recommends postgresql-client
