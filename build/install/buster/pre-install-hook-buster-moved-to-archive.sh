254ca3f771b3cc90c935f0f9589359c3b1c3585f
Iván Todorovich <ivan.todorovich@gmail.com>
2026-05-29 10:55:00 -0300

    🔥 drop unused buster and bullseye install scripts
    These distributions were only used by Odoo 14.0 and 15.0, which were
    dropped in becab7c. The remaining versions use bookworm and trixie.

--- build/install/buster/pre-install-hook-buster-moved-to-archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Debian buster was moved to archive
-sed -i 's,http://deb.debian.org,http://archive.debian.org,g;s,http://security.debian.org,http://archive.debian.org,g' /etc/apt/sources.list
